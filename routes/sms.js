95af41c0f3d05174aaf96fd9b029f1eda7c6bad7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-23 10:43:24 +0800

    Add route for copilot sms service

--- routes/sms.js
+++ routes/sms.js
@@ -15,4 +15,18 @@ router.post('/sms', (req, res) => {
     })
 })
 
+// POST - Send a SMS message using Copilot Messaging Service
+router.post('/smsCopilot', (req, res) => {
+  const attributes = req.body
+
+  sendSMS(attributes.recipient, attributes.message)
+    .then(data => {
+      res.status(201).json({ data })
+    })
+    .catch(error => {
+      res.status(400).json({ error })
+    })
+})
+
+
 module.exports = router
