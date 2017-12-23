8549d2c151f2057d20ce68e3dad553a7ac7efd4b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-23 09:04:15 +0800

    Add function send sms using messaging service

--- twilio/twilioClient.js
+++ twilio/twilioClient.js
@@ -28,6 +28,35 @@ const sendSMS = (to, body) => {
   })
 }
 
+const sendSMSUsingMsgService = (to, body) => {
+  // Initialise account credentials
+  const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+  const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
+  const TWILIO_MESSAGING_SERVICE_SID = process.env.TWILIO_MESSAGING_SERVICE_SID // Using SID instead of Twilio number
+
+  // Create new twilio client
+  const client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+  return new Promise( (success, fail) => {
+    // Send the text message.
+    client.messages.create(
+      {
+        to, // Recipient's number
+        from: TWILIO_NUMBER, // Twilio Number
+        body // Message to Recipient
+      },
+      (error, message) => {
+        if (error) {
+          fail(error)
+        } else {
+          success({to, body})
+        }
+      }
+    )
+  })
+}
+
 module.exports = {
-  sendSMS
+  sendSMS,
+  sendBulkSMS
 }
